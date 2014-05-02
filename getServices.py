6d7a21407bf04db1a59b902e845e55b0ca1dd016
djarrard <djarrard@esri.com>
2014-05-02 15:01:09 -0400

    Enhanced Usability
    The layout of the script was unintuitive. Improved the flow and layout
    of getServices.py to be easier to configure.

--- getServices.py
+++ getServices.py
@@ -1,14 +1,21 @@
 import functions, codecs
 
-#Server Information
-tokenURL = "http://sampleserver6.arcgisonline.com/arcgis/tokens/generateToken"
+#Server Information - specify the REST endpoint and desired URL protocol ("SOAP" or "REST")
 restURL = "http://sampleserver6.arcgisonline.com/arcgis/rest/services"
+protocol = "REST"
+
+#If you wish to use a token, set getToken to true and specify following parameters
+getToken = False
+tokenURL = "http://sampleserver6.arcgisonline.com/arcgis/tokens/generateToken"
 username = "user1"
 password = "user1"
 
 #Get Token and Service List from Server
-token = functions.getToken(tokenURL, username, password)['token']
-serviceList = functions.restCrawl(restURL, token, "SOAP")
+if getToken == True:
+    token = functions.getToken(tokenURL, username, password)['token']
+    serviceList = functions.restCrawl(restURL, token, protocol)
+else:
+    serviceList = functions.restCrawl(restURL, "", protocol)
 
 #Write service list to text file
 log = codecs.open("services.txt",encoding='utf-8', mode="w+")
